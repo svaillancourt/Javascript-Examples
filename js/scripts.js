b9ff4671929ec6ee480795b2803be057cd86d0d5
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-03-28 15:20:21 -0600

    first time

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// // defing step
+// var step = document.querySelector('step');
+
+// // 
+// var myStepButton = document.querySelector( '.menu-button' );
+
+// // Let's listen for a click on this.
+
+// myStepButton.addEventListener('click', function ( event ) {
+//     // When clicked, add/remove the "nav-open" class (in HTML.)
+//     onclick="document.getElementById('step').style.fontSize='50px'";
---
+++ readme.txt
@@ -0,0 +1,1 @@
+this is a compliation of Javascript examples

d3a1e22ea5694498211c4a234c4afcf7c5327072
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-03-29 23:22:55 -0600

    evening update

--- js/assignments.js
+++ js/assignments.js
@@ -9,3 +9,26 @@ var second = [  9, 9, 2, 3, 4];
 document.getElementById("numbers2").innerHTML = second;
 document.getElementById("numbers2Sort").innerHTML = second.sort();
 document.getElementById("numbers2Reverse").innerHTML = second.reverse();
+
+function arrayOfMultiples(num, lenght){
+   // defining the variable
+    var arr = [];
+
+    // first index; i must stop at the desired lenght, incremeneted)
+    for ( i = 1; i <= length; i++){
+        // add the next loop that is multiplied
+        arr.push (num *i );
+    }
+}
+
+// console.log(arrayOfMultiples(7,5));
+
+// const obj = {
+//     a: 'somestring',
+//     b: 42
+// }
+
+// function objectToArray(obj){
+//     var test = Object.entries(obj);
+//     return test;
+// }
